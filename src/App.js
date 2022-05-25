be4256a53b9264c0d038794fb1667df98650568f
janzielinski <jan.zielinski@sinch.com>
2022-05-25 22:05:03 +0200

    Add routing and start styling pages

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import TicketPage from "./pages/TicketPage";
+import NavBar from "./components/Navbar";
+
+function App() {
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <NavBar/>
+                <Routes>
+                    <Route path='/' element={<Dashboard/>}/>
+                    <Route path='/ticket' element={<TicketPage/>}/>
+                    <Route path='/ticket/:id' element={<TicketPage editMode={true}/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default App
